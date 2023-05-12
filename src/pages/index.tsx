0ddde7d6b1a40a45606dd53ff3d95b646adec8c5
orriannafizz <orriannafizz@gmail.com>
2023-05-12 16:50:22 +0800

    build: Upward icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,13 @@
-import { Inter } from "next/font/google";
-import axios from "axios";
-import { useState, useEffect } from "react";
 import Table from "../../components/Table";
 import { ThemeProvider } from "@mui/material";
 import theme from "@/styles/muiTheme";
-const inter = Inter({ subsets: ["latin"] });
+import Upward from "../../components/Upward";
 
 export default function Home() {
   return (
     <ThemeProvider theme={theme}>
       <Table />
+      <Upward />
     </ThemeProvider>
   );
 }
